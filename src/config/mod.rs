1c9f592f5b8eece1a278520898a0f968ac9897fe
张晓华 <wantfulai@163.com>
2025-03-12 22:34:06 +0800

    配置解析v1

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -6,7 +6,7 @@ use nacos_sdk::api::props::ClientProps;
 
 #[allow(unused)]
 pub async fn init_configuration() -> Result<(), Box<dyn std::error::Error>> {
-    let config = get_nacos_configuration().await?;
+    let mut config = get_nacos_configuration().await?;
     println!("nacos config {:?}", config);
     get_configuration_from_remote_server(config).await?;
     Ok(())
@@ -17,21 +17,19 @@ async fn get_configuration_from_remote_server(
     nacos_config: NacosConfig,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let server_properties = nacos_config.nacos;
+    let data_id = server_properties.data_id;
     let config_server = ConfigServiceBuilder::new(
         ClientProps::new()
             .server_addr(server_properties.server_address)
             .namespace(server_properties.namespace)
             .auth_username(server_properties.username)
             .auth_password(server_properties.passwd)
-            .app_name(""),
+            .app_name(&data_id),
     )
     .enable_auth_plugin_http()
     .build()?;
     let config_server_response = config_server
-        .get_config(
-            server_properties.data_id.to_string(),
-            server_properties.data_id.to_string(),
-        )
+        .get_config(data_id, server_properties.group_id.to_string())
         .await?;
     println!("响应信息 {}", config_server_response.content());
     Ok(())
--- src/config/parser.rs
+++ src/config/parser.rs
@@ -4,26 +4,78 @@ use std::fs::File;
 
 #[derive(Debug, Deserialize)]
 pub struct NacosConfig {
+    #[serde(default)]
     pub nacos: NacosProperties,
 }
 
 #[allow(unused)]
 #[derive(Debug, Deserialize, Parser)]
 pub struct NacosProperties {
-    #[serde(default)]
+    #[serde(default = "default_server_address")]
     pub server_address: String,
-    #[serde(default)]
+    #[serde(default = "default_server_port")]
     pub port: u16,
-    #[serde(default)]
+    #[serde(default = "default_nacos_username")]
     pub username: String,
-    #[serde(default)]
+    #[serde(default = "default_naocs_passwd")]
     pub passwd: String,
-    #[serde(default)]
+    #[serde(default = "default_group_id")]
     pub group_id: String,
     #[serde(default)]
     pub namespace: String,
-    #[serde(default)]
+    #[serde(default = "default_data_id")]
     pub data_id: String,
+}
+
+fn default_server_address() -> String {
+    "localhost".to_string()
+}
+
+fn default_server_port() -> u16 {
+    8848
+}
+
+fn default_nacos_username() -> String {
+    Into::<String>::into("nacos")
+}
+
+fn default_naocs_passwd() -> String {
+    Into::<String>::into("nacos")
+}
+
+fn default_group_id() -> String {
+    Into::<String>::into("DEFAULT_GROUP")
+}
+
+fn default_data_id() -> String {
+    String::from("hubbo-sso")
+}
+
+impl Default for NacosConfig {
+    fn default() -> Self {
+        NacosConfig {
+            nacos: NacosProperties::default(),
+        }
+    }
+}
+
+impl Default for NacosProperties {
+    fn default() -> Self {
+        NacosProperties {
+            server_address: default_server_address(),
+            port: default_server_port(),
+            username: default_nacos_username(),
+            passwd: default_naocs_passwd(),
+            data_id: default_data_id(),
+            group_id: default_group_id(),
+            namespace: "".to_string(),
+        }
+    }
+}
+
+/// 解析配置文件中指定的 app name作为配置的data id并区分dev prod环境
+fn parse_app_name() -> String {
+    todo!()
 }
 
 pub async fn get_nacos_configuration() -> Result<NacosConfig, Box<dyn std::error::Error>> {
